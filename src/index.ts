cb40fef473d05832bef64cfd2493387347323980
ComradeAdash <adams@DESKTOP-9VVVG4A.localdomain>
2026-09-06 14:15:14 -0700

    added animation to boxes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+// console.log("test");
+
+// This callback function observves multiple elements at the same time
+// It checks if the element is intersecting with the viewport
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+
+            // allows us to show the animation multiple times when the element is scrolled out of view and back in
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
